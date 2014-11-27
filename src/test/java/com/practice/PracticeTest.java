f0b857c3027b82e68379f5bd95a67314df11ebca
yc03sg2 <yc03sg2@macys.com>
2014-11-27 16:43:52 +0530

    Pushed second time

--- src/test/java/com/practice/PracticeTest.java
+++ src/test/java/com/practice/PracticeTest.java
@@ -1,5 +1,14 @@
 package com.practice;
 
+import org.testng.annotations.*;
+
 public class PracticeTest {
 
+	
+	@Test
+	public void testTrail()
+	{	
+		System.out.print("Hello");
+		System.out.print("Hello second time");
+	}
 }
